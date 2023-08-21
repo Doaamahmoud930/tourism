340c0f227b8dc6b26b50cf254266b3935b9cc558
Doaamahmoud930 <73504396+Doaamahmoud930@users.noreply.github.com>
2023-08-22 01:27:59 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+var btnTooltips=document.querySelector(".btn-tooltip");
+var btnSpan=document.querySelector(".span2");
+
+
+
+btnTooltips.onclick=function(){
+  	alert("Translation copied");
+    btnSpan.style.display="block !important";
+
+  }
+
+
+ 
